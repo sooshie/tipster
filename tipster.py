6f27e268e1b3a97af962c069a5650522870baa06
Mike Sconzo <msconzo@box.com>
2019-11-08 12:53:41 -0600

    minor code cleanup/comments

--- tipster.py
+++ tipster.py
@@ -7,11 +7,16 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk, GLib
 
+
+""" Pop open a text window
+
+The text window has editable text so IOCs can be changed (added/removed/deleted) before sending on
+"""
 def submit_iocs_window(parent, iocs):
     dialogWindow = Gtk.Window()
     dialogWindow.set_title("Submit IOCs")
 
-
+    # What do do when the 'submit' button is clicked
     def on_submit_clicked(button, buff):
         start_iter = buff.get_start_iter()
         end_iter = buff.get_end_iter()
@@ -19,7 +24,7 @@ def submit_iocs_window(parent, iocs):
         submit_iocs = [i.strip() for i in text.split('\n')]
         print(submit_iocs)
 
-
+    # Close the window if the user doesn't want to send the IOCs up
     def on_cancel_clicked(button):
         print("Closing application")
         dialogWindow.destroy()
@@ -53,9 +58,14 @@ def submit_iocs_window(parent, iocs):
     dialogWindow.show_all()
 
 
+""" Main window
+
+Pop up the main window and show a sweet 8bit clippy image. This just shows the program is running, and the icon is 
+awesome.
+"""
 class MainWindow(Gtk.Window):
 
-
+    # Uhhh.. this inits the object
     def __init__(self):
         super(MainWindow, self).__init__()
       
@@ -64,11 +74,15 @@ class MainWindow(Gtk.Window):
         image1.set_from_file("clippy.png")
         self.add(image1)
 
-
+    # Pass through to do the IOC submission 'workflow'
     def submit_iocs(self, iocs):
         return submit_iocs_window(self, iocs)
 
 
+""" Application Main
+
+Spin up the clippy window, and thread that monitors the clipboard.
+"""
 def app_main():
     iocs = []
     clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
@@ -77,7 +91,7 @@ def app_main():
     win.connect("destroy", Gtk.main_quit)
     win.show_all()
 
-
+    # Hack to see if anything was returned from the IOC iterator
     def iter_check(iterable):
         try:
             first = next(iterable)
@@ -85,7 +99,10 @@ def app_main():
             return None
         return itertools.chain([first], iterable)
 
-
+    # Function that polls the clipboard contents every second to look for
+    # 1) New text
+    # 2) if that new text contains any IOCs as identified by iocextract
+    # IOCs are gathered and submitted for user approval
     def check_clippy(iocs):
         last_text = ''
 
@@ -128,6 +145,8 @@ def app_main():
     thread.start()
 
 
+""" Main
+"""
 if __name__ == "__main__":
     app_main()
     Gtk.main()
